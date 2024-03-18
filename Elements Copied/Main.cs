3a6196a796593806e475a4cb11da4012c2435c20
unga <avbjankina@stud.kantiana.ru>
2024-03-18 15:34:02 +0200

    мввм! селект работает, надо отобразить текущий статус (+ почисти дебаг от мусора)

---
+++ Elements Copied/Main.cs
@@ -0,0 +1,39 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.Attributes;
+using System.Collections.Generic;
+using Autodesk.Revit.UI;
+using System;
+
+
+namespace Elements_Copier
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class Plugin : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                UIApplication uiapp = commandData.Application;
+                UIDocument uidoc = uiapp.ActiveUIDocument;
+                Document doc = uidoc.Document;
+
+                SelectionElementsWindow selectElementsWindow = new SelectionElementsWindow(doc, uidoc);
+                //selectElementsWindow.Closed += SelectionWindow_Closed;
+                //selectElementsWindow.ElementSelectionEvent += HandleElementSelection;
+                selectElementsWindow.Show();
+            }
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                return Result.Cancelled;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Elements Copied/View/SelectionElementsWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace Elements_Copier
+{
+    public partial class SelectionElementsWindow : Window
+    {
+        public SelectionElementsWindow(Document doc, UIDocument uidoc)
+        {
+            InitializeComponent();
+            DataContext = new SelectionElementsViewModel(doc, uidoc);
+        }
+    }
+
+}
+
