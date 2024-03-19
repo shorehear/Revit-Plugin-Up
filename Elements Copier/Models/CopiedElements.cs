1d1f1fabbf1741d409a8db71e4ed5dc5916ae5c0
unga <avbjankina@stud.kantiana.ru>
2024-03-19 11:12:05 +0200

    up CopiedElements

--- Elements Copier/Models/CopiedElements.cs
+++ Elements Copier/Models/CopiedElements.cs
@@ -17,5 +17,6 @@ namespace Elements_Copier
             this.uidoc = uidoc;
         }
 
+
     }
 }
--- Elements Copier/View/CopiedElementsWindow.xaml.cs
+++ Elements Copier/View/CopiedElementsWindow.xaml.cs
@@ -1,28 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Elements_Copier
 {
-    /// <summary>
-    /// Логика взаимодействия для CopiedElementsWindow.xaml
-    /// </summary>
-    //public partial class CopiedElementsWindow : UserControl
-    //{
-    //    public CopiedElementsWindow()
-    //    {
-    //        InitializeComponent();
-    //    }
-    //}
+    public partial class CopiedElementsWindow : Window
+    {
+        public CopiedElementsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text == "(X, Y, Z)" || textBox.Text == "0")
+            {
+                textBox.Text = string.Empty;
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                if (textBox == N)
+                {
+                    textBox.Text = "0";
+                }
+                else
+                {
+                    textBox.Text = "(X, Y, Z)";
+                }
+            }
+        }
+    }
+
 }
