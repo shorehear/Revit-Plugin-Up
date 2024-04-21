100e040b68f9a591e62f1b3888ee374654ad51a5
unga <avbjankina@stud.kantiana.ru>
2024-04-21 20:29:22 +0200

    remove usl lib

---
+++ ElementsCopier/Main.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace ElementsCopier
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class Launch : IExternalCommand
+    {
+        private Document doc;
+        private UIDocument uidoc;
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                UIApplication uiapp = commandData.Application;
+                uidoc = uiapp.ActiveUIDocument;
+                doc = uidoc.Document;
+
+                SelectionWindow selectionWindow = new SelectionWindow(doc, uidoc);
+                selectionWindow.Topmost = true;
+                selectionWindow.Show();
+            }
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                return Result.Cancelled;
+            }
+            catch (Exception ex)
+            {
+                TaskDialog.Show("Ошибка", ex.Message);
+                return Result.Failed;
+            }
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ ElementsCopier/Utilities/Logger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ElementsCopier
+{
+    public class PluginLogger
+    {
+        private readonly SelectionElementsViewModel viewModel;
+        public PluginLogger(SelectionElementsViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+
+        public void Log(string message)
+        {
+            viewModel.LogText += message + Environment.NewLine;
+        }
+
+        public void LogInformation(string message)
+        {
+
+            string formattedMessage = $"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}] INFO: {message}";
+            Log(formattedMessage);
+        }
+
+        public void LogWarning(string message)
+        {
+            string formattedMessage = $"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}] WARNING: {message}";
+            Log(formattedMessage);
+        }
+
+        public void LogError(string message)
+        {
+            string formattedMessage = $"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}] ERROR: {message}";
+            Log(formattedMessage);
+        }
+    }
+}
